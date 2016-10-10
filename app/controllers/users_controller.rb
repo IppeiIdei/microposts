f4ca70079475784ef14530238d8d687fdbed7bf3
IPPEI IDEI <ippei.idei@gmail.com>
2016-10-10 07:22:41 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :me?, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
